ccf235d7788117693da1422ee1a7159cd544b6b5
cousinea <cousinea@umich.edu>
2016-01-07 15:55:41 -0500

    Update init.sh

--- skel/init.sh
+++ skel/init.sh
@@ -6,14 +6,14 @@ build=$(cat build)
 
 sed -i -e "s|<containerid>|$(hostname)|g"  \
        -e "s|<build>|$build|g"       \
-       /usr/local/apache2/htdocs/index.html
+       /var/www/html/index.html
 
 PASSVAL=`grep PASSWORD /password | cut -d= -f2`
 
-sed -i "s/PASSWORD/$PASSVAL/g" /usr/local/apache2/htdocs/index.html
+sed -i "s/PASSWORD/$PASSVAL/g" /var/www/html/index.html
 
 ls -l /usr/local/httpd/conf/httpd.conf
 ls -l /usr/local/httpd/conf/extra/httpd-ssl.conf
 ls -l /usr/local/httpd/conf
 
-/usr/local/apache2/bin/httpd -DFOREGROUND -f /usr/local/httpd/conf/httpd.conf
+/usr/sbin/httpd -DFOREGROUND -f /usr/local/httpd/conf/httpd.conf
