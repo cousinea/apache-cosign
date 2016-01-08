e40f0871d15634fb511a2444f3eb029e77dd7e6b
cousinea <cousinea@umich.edu>
2016-01-08 13:47:58 -0500

    Update init.sh

--- skel/init.sh
+++ skel/init.sh
@@ -15,6 +15,7 @@ sed -i "s/PASSWORD/$PASSVAL/g" /var/www/html/index.html
 ls -l /usr/local/httpd/conf/httpd.conf
 ls -l /usr/local/httpd/conf/extra/httpd-ssl.conf
 ls -l /usr/local/httpd/conf
+ls -l /usr/local/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf
 
 #/usr/sbin/httpd -DFOREGROUND -f /usr/local/httpd/conf/httpd.conf
 /usr/sbin/httpd -DFOREGROUND
