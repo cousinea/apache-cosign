536860d5ff032cd894b75eb822ef0fcb5a787de3
cousinea <cousinea@umich.edu>
2016-01-15 15:27:51 -0500

    Update init.sh

--- skel/init.sh
+++ skel/init.sh
@@ -12,14 +12,14 @@ PASSVAL=`grep PASSWORD /password | cut -d= -f2`
 
 sed -i "s/PASSWORD/$PASSVAL/g" /var/www/html/index.html
 
-mkdir -p /usr/local/httpd/conf
-ls -l /usr
-ls -l /usr/local
-ls -l /usr/local/httpd
-ls -l /usr/local/httpd/conf
-ls -l /usr/local/httpd/conf/httpd.conf
-ls -l /usr/local/httpd/conf/extra/httpd-ssl.conf
-cp /usr/local/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf
+#mkdir -p /usr/local/httpd/conf
+#ls -l /usr
+#ls -l /usr/local
+#ls -l /usr/local/httpd
+#ls -l /usr/local/httpd/conf
+#ls -l /usr/local/httpd/conf/httpd.conf
+#ls -l /usr/local/httpd/conf/extra/httpd-ssl.conf
+#cp /usr/local/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf
 
 #/usr/sbin/httpd -DFOREGROUND -f /usr/local/httpd/conf/httpd.conf
 /usr/sbin/httpd -DFOREGROUND
